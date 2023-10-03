f0919e1eb128892835cc1aa1a237e917b7bf524a
Atefeh <atefeh@buzzjective.com>
2023-10-03 20:23:29 +0330

    install router-dom and use it in project, create blogDetails page too

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return ( 
+    <div>
+      <h1>Create new blog</h1>
+    </div>
+  );
+}
+ 
+export default Create;
--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,12 @@ import useFetch from './useFetch'
 
 const Home = () => {
   const {data : blogs, isLoading, error} = useFetch('http://localhost:8000/blogs')
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog)=> blog.id !== id)
-    // setBlogs(newBlogs)
-  }
 
   return (
     <div className="home">
       {error && <div>{error}</div>}
       {isLoading && <div>Loading....</div>}
-      {blogs && <BlogsList blogs={blogs} title='All blogs!' handleDelete={handleDelete}/>}
+      {blogs && <BlogsList blogs={blogs} title='All blogs!'/>}
       {/* <BlogsList blogs={blogs.filter((blog) => blog.author === 'yoshi')} title='Yoshi blogs!'/> */}
     </div>
   );
